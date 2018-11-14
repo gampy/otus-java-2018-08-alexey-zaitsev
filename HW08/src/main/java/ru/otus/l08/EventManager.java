db5bdce52a7b135c55d8c47c7df3427c8cfb8e18
gampy <av_zaitsev@mail.ru>
2018-11-15 00:17:02 +0300

    First commit HW08

--- HW08/src/main/java/ru/otus/l08/EventManager.java
+++ HW08/src/main/java/ru/otus/l08/EventManager.java
@@ -2,10 +2,10 @@ package ru.otus.l08;
 
 public interface EventManager {
 
-    public void subscribe(Object obj, EventListener listener);
+    void subscribe(Object obj, EventListener listener);
 
-    public void unsubscribe(Object obj, EventListener listener);
+    void unsubscribe(Object obj, EventListener listener);
 
-    public void notify(Object obj, int id);
+    void notify(Object obj, int id);
 
 }
